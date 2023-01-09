38f56954e3cf3e66caa843c0325b1d4ad0020907
chaowen guo <chaowen.guo1@gmail.com>
2023-01-09 12:08:02 +0000

    cashzine.sh edited online with Bitbucket

--- cashzine.sh
+++ cashzine.sh
@@ -1,5 +1,23 @@
 apt update
-apt install -y --no-install-recommends curl unzip libgl1
+apt install -y --no-install-recommends curl unzip libgl1 xvfb xauth nodejs
+curl https://deb.nodesource.com/setup_current.x | bash -
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y --no-install-recommends ./google-chrome-stable_current_amd64.deb nodejs
+PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1 npm install playwright-chromium
+Xvfb :99 &
+cat <<EOF | DISPLAY=:99 node --input-type=module
+import {chromium} from 'playwright-chromium'
+const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false})
+const context = await browser.newContext()
+const page = await context.newPage()
+const client = await context.newCDPSession(page)
+await client.send('Emulation.setScriptExecutionDisabled', {value:true})
+await page.goto('https://m.apkpure.com/cashzine-earn-money-reward/com.sky.sea.cashzine/download?from=details')
+const [download] = await globalThis.Promise.all([page.waitForEvent('download'), page.click('a[href="https://d.apkpure.com/b/APK/com.sky.sea.cashzine?version=latest"]')])
+await download.saveAs('cashzine.apk');
+await client.send('Emulation.setScriptExecutionDisabled', {value:false})
+await browser.close()
+EOF
 curl https://dl.google.com/android/repository/commandlinetools-linux-9123335_latest.zip -o commandline.zip
 unzip commandline.zip
 rm -rf commandline.zip
@@ -32,14 +50,14 @@ rm -rf com.termux_118.apk
 sdk/platform-tools/adb install cashzine.apk
 #adb exec-out dumpsys activity | awk /mCurrentFocus/
 sdk/platform-tools/adb exec-out 'am start -n com.termux/com.termux.app.TermuxActivity
-sleep 30
-/data/data/com.termux/files/usr/bin/gawk -v RS=\\n{10} {print\ gensub\(/\\xb4\\x00\\x00\\x00/\,\"\\xff\\xff\\xff\\xff\"\,20\)} /system/bin/screenrecord | /data/data/com.termux/files/usr/bin/head -c -1 > /data/local/tmp/screenrecord
+sleep 1m
+/data/data/com.termux/files/usr/bin/gawk -vRS=\\n{10} {print\ gensub\(/\\xb4\\x00\\x00\\x00/\,\"\\xff\\xff\\xff\\xff\"\,20\)} /system/bin/screenrecord | /data/data/com.termux/files/usr/bin/head -c -1 > /data/local/tmp/screenrecord
 mv /data/local/tmp/screenrecord /system/bin
 tap()
 {
-    sleep 10
+    sleep 20
     uiautomator dump /data/local/tmp/ui.xml
-    array=($(awk -v RS=\> -F= /$1/{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
+    array=($(awk -vRS=\> -vPattern="$1" -F= \$0~Pattern{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
     input tap $(($((${array[0]} + ${array[2]})) / 2)) $(($((${array[1]} + ${array[3]})) / 2))
 }
 /system/bin/linker64 /system/bin/screenrecord /data/local/tmp/cashzine.mp4 &
@@ -59,20 +77,20 @@ array=($(wm size | awk {sub\(/x/\,\"\ \"\,\$NF\)\;print\$NF}))
 halfWidth=$((${array[0]} / 2))
 height=${array[1]}
 input tap $halfWidth $((height / 2))
-while true
+for k in $(seq 0 40)
 do
     uiautomator dump /data/local/tmp/ui.xml
-    icon=($(awk -v RS=\> -F= /icon/{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
+    icon=($(awk -vRS=\> -F= /icon/{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
     for i in $(seq 0 1)
     do
         for j in $(seq 0 1)
         do
-            sleep 10
+	    sleep 10
             input swipe $halfWidth $(($((height / 10)) * 9)) $halfWidth $((height / 10))
         done
         for j in $(seq 0 1)
         do
-            sleep 10
+	    sleep 10
             input swipe $halfWidth $((height / 10)) $halfWidth $(($((height / 10)) * 9))
         done
     done
