5255385d0fa3762c08c359e7c2bea8cadff30777
chaowen guo <chaowen.guo1@gmail.com>
2023-01-13 05:34:37 +0000

    modeEarn.sh edited online with Bitbucket

--- modeEarn.sh
+++ modeEarn.sh
@@ -30,22 +30,18 @@ ssh -fNT -D 1080 -oServerAliveInterval=5 -oStrictHostKeyChecking=no -oProxyComma
 sdk/emulator/emulator -avd android -no-window -no-snapshot -no-audio -no-boot-anim -writable-system -memory 4096 -gpu swiftshader_indirect -http-proxy http://localhost:8118 &
 sdk/platform-tools/adb wait-for-device
 sdk/platform-tools/adb root
-curl -x http://localhost:8118 https://ifconfig.me
 while [[ $(sdk/platform-tools/adb exec-out getprop sys.boot_completed) != 1 ]]
 do
     sleep 30
 done
-curl -x http://localhost:8118 https://ifconfig.me
 sdk/platform-tools/adb shell avbctl disable-verification
 sdk/platform-tools/adb reboot
 sdk/platform-tools/adb wait-for-device
 sdk/platform-tools/adb root
-curl -x http://localhost:8118 https://ifconfig.me
 while [[ $(sdk/platform-tools/adb exec-out getprop sys.boot_completed) != 1 ]]
 do
     sleep 30
 done
-curl -x http://localhost:8118 https://ifconfig.me
 sdk/platform-tools/adb exec-out getprop sys.boot_completed
 sdk/platform-tools/adb remount
 sdk/platform-tools/adb devices -l
@@ -54,7 +50,6 @@ sdk/platform-tools/adb install com.termux_118.apk
 rm -rf com.termux_118.apk
 sdk/platform-tools/adb install modeEarn.apk
 #adb exec-out dumpsys activity | awk /mCurrentFocus/
-curl -x http://localhost:8118 https://ifconfig.me
 sdk/platform-tools/adb exec-out 'am start -n com.termux/com.termux.app.TermuxActivity
 sleep 1m
 /data/data/com.termux/files/usr/bin/gawk -vRS=\\n{10} {print\ gensub\(/\\xb4\\x00\\x00\\x00/\,\"\\xff\\xff\\xff\\xff\"\,20\)} /system/bin/screenrecord | /data/data/com.termux/files/usr/bin/head -c -1 > /data/local/tmp/screenrecord
