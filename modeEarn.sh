942512538db37891a8eca85f3ad7f23fb6be0478
chaowen guo <chaowen.guo1@gmail.com>
2023-01-13 02:29:07 +0000

    modeEarn.sh edited online with Bitbucket

--- modeEarn.sh
+++ modeEarn.sh
@@ -26,6 +26,7 @@ echo y | sdk/cmdline-tools/latest/bin/sdkmanager system-images\;android-30\;goog
 echo no | sdk/cmdline-tools/latest/bin/avdmanager create avd -f -n android -k system-images\;android-30\;google_apis\;x86_64
 ssh -fNT -D 1080 -oStrictHostKeyChecking=no -oProxyCommand='ssh -oStrictHostKeyChecking=no -T guest@ssh.devcloud.intel.com' u180599@devcloud
 echo 'forward-socks5t   /  127.0.0.1:1080 .' | tee -a /etc/privoxy/config
+service privoxy start
 curl -x http://localhost:8118 https://ifconfig.me
 DISPLAY=:99 sdk/emulator/emulator -avd android -no-snapshot -no-audio -no-boot-anim -writable-system -memory 4096 -gpu swiftshader_indirect -http-proxy http://localhost:8118 &
 sdk/platform-tools/adb wait-for-device
