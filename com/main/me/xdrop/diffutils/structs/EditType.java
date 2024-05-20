82503a7d3483112b542582c6b597523dd91d3cfc
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-20 01:36:40 -0700

    Delete com/main/me/xdrop directory

--- com/main/me/xdrop/diffutils/structs/EditType.java
+++
@@ -1,9 +0,0 @@
-package me.xdrop.diffutils.structs;
-/* loaded from: classes10.dex */
-public enum EditType {
-    DELETE,
-    EQUAL,
-    INSERT,
-    REPLACE,
-    KEEP
-}
--- com/main/me/xdrop/fuzzywuzzy/Applicable.java
+++
@@ -1,5 +0,0 @@
-package me.xdrop.fuzzywuzzy;
-/* loaded from: classes10.dex */
-public interface Applicable {
-    int apply(String str, String str2);
-}
