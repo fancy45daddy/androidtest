4a61045cd8f920a7717d461d3abd4ca3d302ce90
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-21 00:44:41 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends android.app.Activity
             for (final $ href:document.select("a.videostream__link.link").stream().map($ -> $.attr("abs:href")).iterator())
             {
                 final var rumble = org.jsoup.Jsoup.connect($).get();
-                android.util.Log.v("1", rumble.select("link[type='application/json+oembed']").attr("href"))
+                android.util.Log.v("1", rumble.select("link[type='application/json+oembed']").attr("href"));
             }
             webView.loadDataWithBaseURL(null, document.title(), "text/html",  "utf-8", null);
         }
