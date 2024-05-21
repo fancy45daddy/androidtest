e0a6063159a036c460072d966e8ebebcfefe381e
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-20 22:48:57 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -8,12 +8,11 @@ public class MainActivity extends android.app.Activity
         super.onCreate(savedInstanceState);
         final var webView = new android.webkit.WebView(this);
         super.setContentView(webView);
-        webView.loadDataWithBaseURL(null, "haha", "text/html",  "utf-8", null);
         try
         {
             try (final var a = new java.net.URL("https://rumble.com/user/chaowenguo1").openConnection().getInputStream())
             {
-                android.util.Log.v("1", new java.lang.String(a.readAllBytes()));
+                webView.loadDataWithBaseURL(null, new java.lang.String(a.readAllBytes()), "text/html",  "utf-8", null)
             }
         }
         catch (final java.lang.Exception e){}
