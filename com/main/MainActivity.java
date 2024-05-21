0067bf5ba2f5c51c7639d3eb153063b27cb2128e
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-20 20:12:14 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -8,8 +8,7 @@ public class MainActivity extends android.app.Activity
         super.onCreate(savedInstanceState);
         final var webView = new android.webkit.WebView(this);
         webView.loadUrl("file:///android_asset/index.html");
-        new java.lang.String(new java.net.URL("http://www.android.com").openConnection().getInputStream().readAllBytes());
-
+        new java.lang.String(new java.net.URL("https://httpbin.org/ip").openConnection().getInputStream().readAllBytes());
         super.setContentView(webView);
     }  
 }
