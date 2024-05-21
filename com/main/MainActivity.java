b724d405a595e2589f90dfc04f97da36378fa712
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-21 01:05:07 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends android.app.Activity
             {
                 android.util.Log.v("1", $);
                 final var rumble = org.jsoup.Jsoup.connect($).get();
-                android.util.Log.v("1", rumble.select("link[type='application/json+oembed']").attr("href"));
+                //android.util.Log.v("1", rumble.select("link[type='application/json+oembed']").attr("href"));
             }
             webView.loadDataWithBaseURL(null, document.title(), "text/html",  "utf-8", null);
         }
