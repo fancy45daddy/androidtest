b5b0dafc7e2f39eb79fd439d6342c38b858867cf
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-21 02:24:57 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -14,12 +14,10 @@ public class MainActivity extends android.app.Activity
             {
                 final var rumble = org.jsoup.Jsoup.connect($).get();
                 final var video = new java.net.URI(rumble.select("link[type='application/json+oembed']").attr("href")).getQuery().split("/");
-                org.jsoup.Jsoup.connect($).get()
-                const url = await axios.get('https://rumble.com/embedJS/u3', {params:{request:'video', ver:2, v:video, ext:globalThis.encodeURIComponent('{"ad_count":null}'), ad_wt:1}}).then(_ => _.data.a.ads.at(0).waterfall.at(0).url)
+                android.util.Log.v("1", java.util.Map.ofEntries(java.util.Map.entry("request", "video"), java.util.Map.entry("ver", 2), java.util.Map.entry("v", video[video.length - 2]), java.util.Map.entry("ext", java.net.URLEncoder.encode("{\"ad_count\":null}")), java.util.Map.entry("ad_wt", 1)));
+                //const url = await axios.get('https://rumble.com/embedJS/u3', {params:{request:'video', ver:2, v:video, ext:globalThis.encodeURIComponent('{"ad_count":null}'), ad_wt:1}}).then(_ => _.data.a.ads.at(0).waterfall.at(0).url)
 
 
-                video = video[video.length - 2];
-                android.util.Log.v("1", video);
             }
             webView.loadDataWithBaseURL(null, document.title(), "text/html",  "utf-8", null);
         }
