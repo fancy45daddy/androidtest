e38554e349a2781a41fce1867beb0fbd2fccaa77
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-20 21:22:18 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -1,43 +1,23 @@
 package com.main;
 
-public class MainActivity extends AppCompatActivity {
-
-    private WebView mWebView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends android.app.Activity   
+{
+    @Override  
+    public void onCreate(final android.os.Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar mToolbarTb = (Toolbar) findViewById(R.id.tb_toolbar);
-        setSupportActionBar(mToolbarTb);
-
-        mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("file:///android_asset/index.html");
-        mWebView.addJavascriptInterface(MainActivity.this, "main");
-
-        mWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                return super.onJsAlert(view, url, message, result);
+        final var webView = new android.webkit.WebView(this);
+        super.setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        webView.loadUrl("javascript:javaCallJs(" + "'Message From Java'" + ")");
+        /*try
+        {
+            try (final var a = new java.net.URL("https://httpbin.org/ip").openConnection().getInputStream())
+            {
+                new java.lang.String(a.readAllBytes());
             }
-        });
-    }
-
-    public void javaCallJs(View v){
-        mWebView.loadUrl("javascript:javaCallJs(" + "'Message From Java'" + ")");
-    }
-
-    @JavascriptInterface
-    public void jsCallJava(String message){
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
+        }
+        catch (final java.lang.Exception e){}*/
+    }  
 }
