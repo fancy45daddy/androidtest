620534d5877f01798dfe8adc97685726122470f5
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-20 20:36:06 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -3,15 +3,19 @@ package com.main;
 public class MainActivity extends android.app.Activity   
 {
     @Override  
-    public void onCreate(final android.os.Bundle savedInstanceState) throws java.lang.Exception
+    public void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         final var webView = new android.webkit.WebView(this);
         webView.loadUrl("file:///android_asset/index.html");
-        try (final var a = new java.net.URL("https://httpbin.org/ip").openConnection().getInputStream())
+        try
         {
-            new java.lang.String(a.readAllBytes());
+            try (final var a = new java.net.URL("https://httpbin.org/ip").openConnection().getInputStream())
+            {
+                new java.lang.String(a.readAllBytes());
+            }
         }
+        catch (final java.lang.Exception e){}
         super.setContentView(webView);
     }  
 }
