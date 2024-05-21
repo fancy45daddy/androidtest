ec7012ebdec0f3973946e00765839d31e78a6bed
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-05-20 23:23:24 -0700

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -8,13 +8,7 @@ public class MainActivity extends android.app.Activity
         super.onCreate(savedInstanceState);
         final var webView = new android.webkit.WebView(this);
         super.setContentView(webView);
-        try
-        {
-            try (final var a = new java.net.URL("https://rumble.com/user/chaowenguo1").openConnection().getInputStream())
-            {
-                webView.loadDataWithBaseURL(null, new java.lang.String(a.readAllBytes()), "text/html",  "utf-8", null);
-            }
-        }
-        catch (final java.lang.Exception e){}
+        final var doc = org.jsoup..Jsoup.connect("https://en.wikipedia.org/").get();
+        webView.loadDataWithBaseURL(null, doc.title(), "text/html",  "utf-8", null);
     }  
 }
